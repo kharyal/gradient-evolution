1c8c1931962ebb0d25c34c9ae98698c9f074bab3
Tanmay Kumar Sinha <tanmaykrsinha@rediffmail.com>
2021-01-02 06:46:46 +0530

    Implemented algorithm on OpenAI Environments

---
+++ geneticGD_openai.py
@@ -0,0 +1,118 @@
+import numpy as np
+import gym
+import time
+import multiprocessing as mp
+
+num_processes = 4
+gen_size = 30
+
+def choose_eta(size):
+	choices = np.arange(size[0])
+	basis_ind = np.random.choice(choices, size[1])
+	eta = np.zeros(size)
+	# print(eta.shape)
+	eta[basis_ind, np.arange(size[1])] = 1
+	return eta
+
+def get_reward(weights,env,shape,ep_max_step=200):
+	s = env.reset()
+	# print(weights.shape)
+	params = params_reshape(weights,shape)
+	# reward = 100.
+	reward = 0.
+	# ss = []
+	for i in range(ep_max_step):
+		a = get_action(params,s,shape)
+		s,r,done, _ = env.step(a)
+		reward += r
+		# ss.append(s)
+		if done:
+			break
+	# return np.max(ss)
+	return reward
+
+def params_reshape(params,shapes):     # reshape to be a matrix
+	p, start = [], 0
+	for i, shape in enumerate(shapes):  # flat params to matrix
+		n_w, n_b = shape[0] * shape[1], shape[1]
+		p = p + [params[start: start + n_w].reshape(shape),
+				 params[start + n_w: start + n_w + n_b].reshape((1, shape[1]))]
+		start += n_w + n_b
+	return p
+
+def build_net(num_features, num_actions):
+	def linear(n_in, n_out):  # network linear layer
+		w = np.random.randn(n_in * n_out).astype(np.float32) * .1
+		b = np.random.randn(n_out).astype(np.float32) * .1
+		return (n_in, n_out), np.concatenate((w, b))
+	s0, p0 = linear(num_features, 30)
+	s1, p1 = linear(30, 20)
+	s2, p2 = linear(20, num_actions)
+	return [s0, s1, s2], np.concatenate((p0, p1, p2))
+
+def get_action(params,state,shape):
+	# params = params_reshape(weights,shape)
+	x = state[np.newaxis, :]
+	x = np.tanh(x.dot(params[0]) + params[1])
+	x = np.tanh(x.dot(params[2]) + params[3])
+	x = x.dot(params[4]) + params[5]
+	return np.argmax(x, axis=1)[0]
+
+def mutate(parent_weights,parent_reward,env,shape,pool,num_eta = 10,t = 0.001,lr = 0.5):
+	eta = choose_eta((parent_weights.shape[0],num_eta))
+	eta_weights = parent_weights[:,None] + t*eta
+	jobs = [pool.apply_async(get_reward,(eta_weights[:,k], env, shape)) for k in range(num_eta)]
+	rewards = np.array([j.get() for j in jobs])
+	b = (rewards - parent_reward)/t
+	grad,_,_,_ = np.linalg.lstsq(eta.T,b)
+	grad = grad[:,None]
+	child_weights = parent_weights + lr*grad.T
+	return child_weights
+
+def play(weights,env,ep_max_step=1000):
+	s = env.reset()
+	# print(weights.shape)
+	params = params_reshape(weights,shape)
+	reward = 0.
+	for i in range(ep_max_step):
+		env.render()
+		a = get_action(params,s,shape)
+		s,r,done, _ = env.step(a)
+		reward += r
+		if done:
+			print("Done")
+			break
+	return reward
+
+
+def train(weights, shape, env, pool):
+	max_iteration = 50
+	for i in range(max_iteration):
+		jobs = [pool.apply_async(get_reward,(weight.T,env,shape)) for weight in weights.T]
+		rewards = np.array([j.get() for j in jobs])
+		err = np.array(sorted(zip(rewards,np.arange(gen_size)),reverse = True))
+		weights = weights[:,err[:gen_size//2,1].astype(int)]
+		# jobs = [pool.apply_async(mutate,(weights[:,j],err[j,0],env,shape,pool)) for j in range(weights.shape[1])]
+		# new_weights = np.array([j.get() for j in jobs])
+		new_weights = np.array([mutate(weights[:,j],err[j,0],env,shape,pool) for j in range(weights.shape[1])])
+		new_weights = np.reshape(new_weights,(weights.shape[0],gen_size//2))
+		# print(new_weights.shape)
+		weights = np.hstack((weights,new_weights))
+		# print(weights.shape)
+		# exit()
+		print("Iter: ", i , err[0,0])
+	
+	play(weights[:,0], env, ep_max_step = 10000000)
+
+
+if __name__ == "__main__":
+	pool = mp.Pool(processes = num_processes)
+	env = gym.make('MountainCar-v0')
+	num_features = 2
+	num_actions = 3
+	ep_max_step = 200
+	weights = np.array([build_net(num_features,num_actions)[1] for j in range(gen_size)]).T
+	shape,_ = build_net(num_features,num_actions)
+	# play(weights[:,0],env,ep_max_step = 10000)
+	# print(shape,weights.shape)
+	train(weights,shape,env,pool)
--- genetic_grad_descent_img_renderer.py
+++ genetic_grad_descent_img_renderer.py
@@ -5,87 +5,9 @@ from tqdm import tqdm
 import time
 from copy import deepcopy
 
-im_sz = (128,128)
-im1 = cv2.resize(cv2.imread("1.png",0), im_sz)[:,:,None]/255
-# plt.imshow(np.reshape(im1,im_sz), 'gray')
-# plt.show()
+im_sz = (32,32)
+im1 = cv2.resize(cv2.imread("1.jpg",0), im_sz)[:,:,None]/255
 im1 = np.reshape(im1, (im_sz[0]*im_sz[1],1))
-
-
-# sz = 50
-# gen_img = np.random.uniform(0,1, (im_sz[0],im_sz[1],3, sz))
-
-
-# iterations = 1001
-# with tqdm(iter(range(iterations))) as titer:
-#     for i in titer:
-
-#         pix_err = np.abs((gen_img-im1))
-#         err = np.sum((pix_err)**2, axis=(0,1,2))
-#         err = np.array(sorted(zip(err, np.arange(sz))))
-#         titer.set_postfix(ERROR = err[0,0])
-#         gen_img = gen_img[:,:,:,err[:sz//2,1].astype(int)]
-#         pix_err = pix_err[:,:,:,err[:sz//2,1].astype(int)]
-#         mutated = (gen_img + np.random.normal(0,0.5, (im_sz[0], im_sz[1],3, sz//2)))
-#         mutated = np.clip(mutated, 0, 1)
-#         gen_img = np.concatenate((gen_img, mutated), axis=3)
-#         if i%1 == 0:
-#             # plt.imshow(gen_img[:,:,:,0])
-#             # plt.show()
-#             cv2.imshow('reconstruction', gen_img[:,:,:,0])
-#             cv2.waitKey(1)
-            
-#             # time.sleep(1)
-
-
-# cv2.destroyAllWindows()
-
-# def genetic_grad(im1, size_generation=30, size_eta=50, iterations=10000, lr = 0.5):
-#     #generation
-#     sz = size_generation
-#     gen_img = np.random.uniform(0,1, (im_sz[0]*im_sz[1], sz))
-    
-#     # grad_actual = 0
-#     # grad_pred = 0
-#     with tqdm(iter(range(iterations))) as titer:
-#         for i in titer:
-
-#             pix_err = (gen_img-im1)
-#             err = np.sum((pix_err)**2, axis=0)
-#             err = np.array(sorted(zip(err, np.arange(sz))))
-#             gen_img = gen_img[:,err[:sz//2,1].astype(int)]
-#             gen_img_copy = deepcopy(gen_img)
-#             # print(err)
-#             lr = min(lr, 150/(i+0.1))
-#             for j in range(gen_img_copy.shape[1]):
-#                 eta = np.random.uniform(-0.0000005, 0.0000005, (im_sz[0]*im_sz[1], size_eta))
-#                 img_eta = gen_img[:,j][:,None] + eta
-#                 err_eta = np.sum(np.abs((img_eta - im1))**2, axis = 0)
-#                 b = err_eta - err[j,0]
-#                 # grad,_,_,_ = np.linalg.lstsq(eta.T, b)
-#                 # grad = grad[:,None]
-#                 grad = np.linalg.pinv(eta.dot(eta.T))@eta@b
-#                 # grad = 2*(gen_img[:,j] - im1.T)
-#                 # print(grad.shape)
-#                 # print("AAAAA",(gen_img[:,j] - lr*grad).shape)
-#                 # print("AAAAA",(gen_img[:,j] - lr*grad.T).T.shape)
-#                 gen_img = np.hstack((gen_img, (gen_img[:,j] - lr*grad.T).T))
-#                 # gen_img = np.hstack((gen_img, (gen_img[:,j] - lr*grad).T))
-#                 # print(gen_img.shape)
-#                 # exit()
-#                 # print(gen_img.shape)
-#                 if j == 0:
-#                     grad_pred = grad
-#                     grad_actual = 2*(gen_img[:,j] - im1.T)
-#             # print(grad_pred.shape)
-#             grad_angle = np.arccos(grad_actual.dot(grad_pred)/(np.linalg.norm(grad_actual)*np.linalg.norm(grad_pred)))
-#             titer.set_postfix(ERROR = err[0,0], grad_angle = grad_angle)
-#             # titer.set_postfix(ERROR = err[0,0])
-#             cv2.imshow('reconstruction', np.reshape(gen_img[:,0], (im_sz[0], im_sz[1])))
-#             cv2.waitKey(1)
-# genetic_grad(im1)
-
-
 
 def choose_eta(size):
     choices = np.arange(size[0])
@@ -95,7 +17,7 @@ def choose_eta(size):
     eta[basis_ind, np.arange(size[1])] = 1
     return eta
 
-def genetic_grad(im1, size_generation=30, size_eta=30, iterations=10000, lr = 0.5):
+def genetic_grad(im1, size_generation=30, size_eta=15, iterations=10000, lr = 0.5):
     #generation
     sz = size_generation
     gen_img = np.random.uniform(0,1, (im_sz[0]*im_sz[1], sz))
@@ -114,23 +36,13 @@ def genetic_grad(im1, size_generation=30, size_eta=30, iterations=10000, lr = 0.
             lr = min(lr, 400/(i+0.1))
             t = 0.001
             for j in range(gen_img_copy.shape[1]):
-
-                # eta = np.random.uniform(-1, 1, (im_sz[0]*im_sz[1], size_eta))
                 eta = choose_eta((im_sz[0]*im_sz[1], size_eta))
-                # eta = eta[:, :size_eta]
                 img_eta = gen_img[:,j][:,None] + t*eta
                 err_eta = np.sum((img_eta - im1)**2, axis = 0)
                 b = (err_eta - err[j,0])/t
                 grad,_,_,_ = np.linalg.lstsq(eta.T, b)
                 grad = grad[:,None]
-                # print(grad.shape)
-                # print("AAAAA",(gen_img[:,j] - lr*grad).shape)
-                # print("AAAAA",(gen_img[:,j] - lr*grad.T).T.shape)
-                # print(gen_img.shape)
                 gen_img = np.hstack((gen_img, (gen_img[:,j] - lr*grad.T).T))
-                # gen_img = np.hstack((gen_img, (gen_img[:,j] - lr*grad).T))
-                # print(gen_img.shape)
-                # exit()
                 if j == 0:
                     grad_pred = grad
                     grad_actual = 2*(gen_img[:,j] - im1.T)
